7b2d0678f8ed5a7d141e745ff48dd62ee6fd68eb
Gaurav Gurjar <ggurjar333@gmail.com>
2024-03-25 17:31:18 +0530

    adds localstack and run.sh file.

--- localstack-run.sh
+++ localstack-run.sh
@@ -14,3 +14,14 @@ awslocal ec2 authorize-security-group-ingress \
     --cidr 0.0.0.0/0
 
 sg_id=$(awslocal ec2 describe-security-groups | jq -r '.SecurityGroups[0].GroupId')
+
+awslocal ec2 run-instances \
+    --image-id ami-ff0fea8310f3 \
+    --count 1 \
+    --instance-type t3.nano --key-name my-key \
+    --security-group-ids $sg_id \
+    --user-data file://./run.sh
+
+list_instances=$(awslocal ec2 describe-instances | jq -r '.Reservations[0].Instances[0].InstanceId')
+
+awslocal ec2 terminate-instances --instance-id $list_instances
