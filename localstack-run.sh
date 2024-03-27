912bd827150c6141f82b8639b56d245ad90caf75
Gaurav Gurjar <ggurjar333@gmail.com>
2024-03-27 16:58:15 +0530

    updates to the image id.

--- localstack-run.sh
+++ localstack-run.sh
@@ -10,18 +10,17 @@ chmod 400 key.pem
 awslocal ec2 authorize-security-group-ingress \
     --group-id default \
     --protocol tcp \
-    --port 8000 \
+    --port 22 \
     --cidr 0.0.0.0/0
 
 sg_id=$(awslocal ec2 describe-security-groups | jq -r '.SecurityGroups[0].GroupId')
+list_instances=$(awslocal ec2 describe-instances | jq -r '.Reservations[0].Instances[0].InstanceId')
 
 awslocal ec2 run-instances \
-    --image-id ami-ff0fea8310f3 \
+    --image-id $list_instances \
     --count 1 \
     --instance-type t3.nano --key-name my-key \
     --security-group-ids $sg_id \
     --user-data file://./run.sh
 
-list_instances=$(awslocal ec2 describe-instances | jq -r '.Reservations[0].Instances[0].InstanceId')
-
-awslocal ec2 terminate-instances --instance-id $list_instances
+#awslocal ec2 terminate-instances --instance-id $list_instances
